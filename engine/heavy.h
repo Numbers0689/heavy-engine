fe08c4b4fb3cc706eceed87763e4c109b784d8d3
Numbers0689 <firewatchsai@gmail.com>
2023-11-26 16:33:59 +0530

    Add lib file teamplate

---
+++ engine/heavy.h
@@ -0,0 +1,11 @@
+#ifndef HEAVY_H
+#define HEAVY_H
+
+typedef unsigned long long U64;
+
+#define NAME "Carmin 1.0"
+#define BOARD_SQ_NUM 120
+
+enum { EMPTY, wP, wN, wB, wR, wQ, wK, bP, bN, bB, bR, bQ, bK };
+
+#endif
