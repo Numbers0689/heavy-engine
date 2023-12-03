dcc1f2cfdaecf4a4876d307711c3c80fa493527d
Numbers0689 <firewatchsai@gmail.com>
2023-12-03 18:41:23 +0530

    Add set and clear bits operations

--- engine/heavy.h
+++ engine/heavy.h
@@ -88,12 +88,16 @@ typedef struct
 // globals
 extern int Sq120ToSq64[BOARD_SQ_NUM];
 extern int Sq64ToSq120[64];
+extern U64 SetMask[64];
+extern U64 ClearMask[64];
 
 // macros
 #define FR2SQ(f, r) ((21 + (f)) + ((r) * 10))
 #define SQ64(sq120) Sq120ToSq64[sq120]
 #define POP(b) PopBit(b)
 #define CNT(b) CountBits(b) 
+#define CLRBIT(bb, sq) ((bb) &= ClearMask[(sq)])
+#define SETBIT(bb, sq) ((bb) != SetMask[(sq)])
 
 // functions 
 extern void AllInit();
